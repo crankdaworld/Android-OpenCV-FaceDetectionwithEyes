90299294fe3c9e3fe4bd1ae5cbdea001d4dab528
Imran Akthar <imranakthar@gmail.com>
2013-04-20 12:08:20 +0530

    First Commit

---
+++ AndroidOpenCVFaceDetectwithEye/FdEye/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,139 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/assert.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdlib.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/alloca.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/strings.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdint.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/memory.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/math.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/limits.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/limits.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/limits.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/limits.h \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/jni.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/assert.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdlib.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/string.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/alloca.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/strings.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/memory.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/math.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/limits.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/limits.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/linux/limits.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/machine/limits.h:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+C:/android-ndk-r8b-windows/android-ndk-r8b/platforms/android-14/arch-arm/usr/include/android/log.h:
---
+++ AndroidOpenCVFaceDetectwithEye/FdEye/src/org/opencv/samples/fd/HeadPose.java
@@ -0,0 +1,56 @@
+package org.opencv.samples.fd;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
+public class HeadPose
+{
+	public float PI=3.141592f;
+	public enum HeadPoseStatus { NONE, KEYFRAME, TRACKING };
+	public int cornerCount,maxCorners,focalLength;
+	//Point modelPoints;
+	public List<Point> modelPoints = new ArrayList<Point>();
+	public List<Point> previousCorners = new ArrayList<Point>();
+	public List<Point> corners = new ArrayList<Point>();
+	public HeadPoseStatus status;
+	public Mat translationVector,rotationMatrix,cropped;
+	
+	public HeadPose()
+	{
+		focalLength=600;
+		cornerCount=0;
+		maxCorners=100;
+		//previousCorners=NULL;
+		
+		
+		
+		
+	}
+	public void HeadPoseDetection(Mat mRgba,Rect[] facesArray)
+	{
+		this.status=status.NONE;
+		
+		Rect roi = new Rect(facesArray[0].x, facesArray[0].y, facesArray[0].height,facesArray[0].width);
+		cropped = new Mat(mRgba, roi);
+		//roi=mRgba.submat(facesArray[0].x,facesArray[0].br(),facesArray[0].y,facesArray[0].tl());
+		hpFindCorners(mRgba,facesArray);
+		
+		
+	}
+	
+	public void hpFindCorners(Mat mRgba,Rect[] facesArray)
+	{
+		Mat faceG= new Mat(cropped.height(),cropped.width(), CvType.CV_8U);
+		Mat eigImage= new Mat(cropped.height(),cropped.width(), CvType.CV_32F);
+		Mat tmpImage= new Mat(cropped.height(),cropped.width(), CvType.CV_32F);
+		Imgproc.cvtColor(mRgba, faceG,Imgproc.COLOR_RGB2GRAY);
+		///Imgproc.goodFeaturesToTrack(faceG, eigImage, tmpImage, qualityLevel, minDistance);
+	}
+	
+	
+	
+}
